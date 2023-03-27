f51c80d24694ec0874455d2aa5fed12504801153
juan pablo <chito.pepsi@gmail.com>
2023-03-27 17:10:02 -0600

    ejercicios js terminados

--- js/main.js
+++ js/main.js
@@ -27,3 +27,56 @@ for (let i = 0; i < nuevosNombres.length; i++) {
 for (let i = 0; i < nombres.length; i++) {
   console.log(nombres[i]);
 }
+
+// #ejercicio arbol
+for (let i = 1; i <= 5; i++) {
+  let espacio = "";
+  for (let bola = 1; bola <= i; bola++) {
+    espacio += "* ";
+  }
+  console.log(espacio);
+}
+
+//   #while exercise
+let x = 5;
+
+while (x > 0) {
+  console.log(x);
+  x -= 0.5;
+}
+// 2
+let num = 1;
+
+while (num <= 100) {
+  if (num % 2 === 1) {
+    console.log(num);
+  }
+  num++;
+}
+// 3
+function addParentesis(numeros) {
+  let num = 1;
+  let resultado = "";
+
+  while (num <= numeros) {
+    resultado += "[" + num + "] ";
+    num++;
+  }
+
+  console.log(resultado);
+}
+
+addParentesis(8);
+// 4
+function sumarNumeros(numerosASumar) {
+  let suma = 0;
+  let i = 1;
+
+  while (i <= numerosASumar) {
+    suma += i;
+    i++;
+  }
+
+  return suma;
+}
+console.log(sumarNumeros(6));
